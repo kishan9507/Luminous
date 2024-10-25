d1307c7283653e3368588e6eac3c3f35e9fa14a8
KISHAN KUMAR <132150300+kishan9507@users.noreply.github.com>
2024-10-26 00:48:30 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,7 @@ import { Container } from '@mui/material';
 function App() {
   return (
     <Container maxWidth="lg">
-      <h1 style={{ textAlign: 'center' }}>Energy Optimization Platform</h1>
+      <h1 style={{ textAlign: 'center',backgroundColor:'green' }}>Energy Optimization Platform</h1>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
